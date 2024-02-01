910c4f102b7e339cf160675b4936401fd13e8c81
Samuel Rigaud <samuel.rigaud@etu.unilim.fr>
2024-02-01 09:29:14 +0100

    Objection and Schwifty done

--- server/manifest.js
+++ server/manifest.js
@@ -38,25 +38,24 @@ module.exports = new Confidence.Store({
                 plugin: './plugins/swagger'
             },
             {
-                plugin: '@hapipal/schwifty',
-                options: {
-                    $filter: 'NODE_ENV',
-                    $default: {},
-                    $base: {
-                        migrateOnStart: true,
-                        knex: {
-                            client: 'sqlite3',
-                            useNullAsDefault: true,     // Suggested for sqlite3
-                            connection: {
-                                filename: ':memory:'
-                            },
-                            migrations: {
-                                stub: Schwifty.migrationsStubPath
+                plugin  : '@hapipal/schwifty',
+                options : {
+                    $filter    : 'NODE_ENV',
+                    $default   : {},
+                    $base      : {
+                        migrateOnStart : true,
+                        knex           : {
+                            client     : 'mysql',
+                            connection : {
+                                host     : process.env.DB_HOST || '0.0.0.0',
+                                user     : process.env.DB_USER || 'root',
+                                password : process.env.DB_PASSWORD || 'samuel23',
+                                database : process.env.DB_DATABASE || 'tp_node'
                             }
                         }
                     },
-                    production: {
-                        migrateOnStart: false
+                    production : {
+                        migrateOnStart : false
                     }
                 }
             },
